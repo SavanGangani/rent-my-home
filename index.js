0ffe561309c560ded8601b9d159d1cc5340ca9b4
dhrumit-sutariya <pateldhrumit4@gmail.com>
2023-07-25 22:15:40 +0530

    Updated

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 const userRoutes = require('./api/routes/userRoute');
 const postRoutes = require('./api/routes/postRoute');
+
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/RentmyHome', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://RentMyHome:RentMyHome@cluster0.k048sbq.mongodb.net/RentMyHome?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to database');
     })
     .catch(() => {
         console.log('Connection failed');
     });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
